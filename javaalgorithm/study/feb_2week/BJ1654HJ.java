974629e55f22f6bf9efdf1220e9068e603c508b5
HYEJIN <vvloveletter@naver.com>
2024-02-08 23:13:20 +0900

    0208

--- javaalgorithm/study/feb_2week/BJ1654HJ.java
+++ javaalgorithm/study/feb_2week/BJ1654HJ.java
@@ -1,4 +1,51 @@
 package study.feb_2week;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class BJ1654HJ {
+    static int k, n;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        k = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        arr = new int[k];
+        for (int i=0; i<k; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        System.out.println(binarySearch());
+
+    }
+    //탐색 범위를 의미하는 변수들은 long으로 선언해야 합니다.
+    //그렇지 않으면 1 + (2^31-1) 에서 오버플로가 발생하기 때문입니다
+    public static long binarySearch(){
+        long start = 1;
+        long end = arr[arr.length-1];
+        long mid, cnt;
+
+        while (start<=end){
+            mid = (start+end)/2;
+            cnt = 0;
+
+            for (int i=0; i<k; i++){
+                cnt += arr[i]/mid;
+            }
+
+            if (cnt >= n){
+                start = mid+1;
+            }else {
+                end = mid-1;
+            }
+        }
+        return end;
+    }
 }
--- javaalgorithm/study/feb_2week/BJ19939HJ.java
+++ javaalgorithm/study/feb_2week/BJ19939HJ.java
@@ -1,4 +1,36 @@
 package study.feb_2week;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BJ19939HJ {
+    static int n, k, sum, res;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        // 바구니에 순차적(한개씩 늘어나게)으로 공을 넣음
+        for (int i=1; i<=k; i++){
+            sum+=i;
+        }
+        n -= sum;
+        // 바구니에 공의 개수가 같은 경우가 있음
+        if (n<0){
+            res = -1;
+        } else {
+            // 바구니에 든 각 공의 갯수가 1개씩 차이남 (n-(n-k+1))
+            if (n%k == 0){
+                res = k-1;
+            // 공이 남으면 공이 가장 많이 든 바구니부터 채워나가야함
+            // (n+1)-(n-k+1)
+            }else{
+                res = k;
+            }
+        }
+        System.out.println(res);
+    }
 }
