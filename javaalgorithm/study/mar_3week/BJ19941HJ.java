51ebe99e73ea24c5f253af549aaf2da4edcdd4a0
HYEJIN <vvloveletter@naver.com>
2024-03-18 21:34:20 +0900

    0318

--- javaalgorithm/study/mar_3week/BJ19941HJ.java
+++ javaalgorithm/study/mar_3week/BJ19941HJ.java
@@ -13,10 +13,14 @@ public class BJ19941HJ {
         k = Integer.parseInt(st.nextToken());
         input = br.readLine().split("");
         for (int i=0; i<n; i++){
+            // 사람일 때
             if (input[i].equals("P")){
+                // 사람기준 K만큼 떨어진 거리
                 for (int j=i-k; j<=i+k; j++){
+                    // 범위를 벗어나면 pass
                     if (j<0 || j>=n) continue;
                     if (input[j].equals("H")){
+                        // 중복 체크되는 것을 막기 위해 다른 문자열로 바꿔줌
                         input[j] = "A";
                         ans ++;
                         break;
--- javaalgorithm/study/mar_3week/BJ20006HJ.java
+++ javaalgorithm/study/mar_3week/BJ20006HJ.java
@@ -12,6 +12,7 @@ public class BJ20006HJ {
             this.level = level;
             this.name = name;
         }
+        // Plyaer 사전식 정렬
         @Override
         public int compareTo(Player player){
             return name.compareTo(player.name);
@@ -24,6 +25,7 @@ public class BJ20006HJ {
         p = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         Player[] players = new Player[p];
+        // Plyaer를 배열에 입력
         for (int i=0; i<p; i++){
             String[] input = br.readLine().split(" ");
             lv = Integer.parseInt(input[0]);
@@ -35,11 +37,13 @@ public class BJ20006HJ {
             ArrayList<Player> room = new ArrayList<>();
             if (!players[i].chk){
                 for (int j=i; j<p; j++){
+                    // room의 크기가 m이면 (플레이어가 m명인 것) break
                     if (room.size()==m){
                         break;
                     }
                     int level = players[j].level;
                     String name = players[j].name;
+                    // 입장한 플레이어의 레벨을 기준으로 -10부터 +10까지 입장 가능
                     if (!players[j].chk && players[i].level-10<=level && players[i].level+10>=level){
                         players[j].chk = true;
                         room.add(new Player(level, name));
