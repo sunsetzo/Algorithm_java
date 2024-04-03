be66d3e8f6c6cd30d9f259dba8f8036703258e2b
HYEJIN <vvloveletter@naver.com>
2024-04-03 21:13:37 +0900

    0403

---
+++ javaalgorithm/study/apr_1week/BJ2512HJ.java
@@ -0,0 +1,41 @@
+package study.apr_1week;
+
+import java.io.*;
+import java.util.*;
+public class BJ2512HJ {
+    static int n, m;
+    static long ans;
+    static int[] arr;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        for (int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        m = Integer.parseInt(br.readLine());
+        long start = 1; long end = arr[arr.length-1];
+
+        while (start <= end){
+            long mid = (start+end)/2;
+            long sum = 0;
+            for (int i=0; i<n; i++){
+                if (arr[i] >= mid){
+                    sum += mid;
+                }else{
+                    sum += arr[i];
+                }
+            }
+            if (sum > m){
+                end = mid-1;
+            }else{
+                ans = Math.max(ans, mid);
+                start = mid+1;
+            }
+        }
+        System.out.println(ans);
+
+    }
+}
