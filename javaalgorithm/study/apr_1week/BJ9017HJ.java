fc8d0f010c0df6c7e4d649cb019bee3ac26787ff
HYEJIN <vvloveletter@naver.com>
2024-04-07 22:23:14 +0900

    0407

--- javaalgorithm/study/apr_1week/BJ9017HJ.java
+++ javaalgorithm/study/apr_1week/BJ9017HJ.java
@@ -17,13 +17,15 @@ public class BJ9017HJ {
             for (int i=0; i<n; i++){
                 info[i] = Integer.parseInt(st.nextToken());
             }
+            // 0 : 출전 선수, 1 : 점수의 합, 2 : 4명까지 확인했는지 체크, 3: 5번째 선수 점수
             team = new int[201][4];
+            // 출전 선수 카운트
             for (int i : info) {
                 team[i][0] += 1;
             }
             int score = 1;
-            int ans = Integer.MAX_VALUE;
-            int res = 0;
+            int ans = Integer.MAX_VALUE;    // 점수의 합
+            int res = 0;    // 팀 넘버
 
             for (int i : info){
                 if (team[i][0] >= 6){
