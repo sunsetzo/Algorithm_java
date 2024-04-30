f3f15c19550a6cd1c18c6c34bfc9e05100896e08
HYEJIN <vvloveletter@naver.com>
2024-04-30 23:03:56 +0900

    0430

--- javaalgorithm/study/apr_4week/BJ1446HJ.java
+++ javaalgorithm/study/apr_4week/BJ1446HJ.java
@@ -7,6 +7,7 @@ public class BJ1446HJ {
     static int n, d;
     static int[] dp;
     static int[][] arr;
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,31 +15,31 @@ public class BJ1446HJ {
         d = Integer.parseInt(st.nextToken());
         arr = new int[n][3];
 
+        // 지름길 정보 입력 받기
         for (int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             for (int j=0; j<3; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken()); // 시작점, 끝점, 지름길의 길이 입력 받음
             }
         }
 
-        dp = new int[d+1];
+        dp = new int[d+1]; // 목적지까지 각 거리를 저장할 배열 초기화, 거리는 0부터 d까지이므로 d+1 크기 할당
         for (int i=0; i<=d; i++){
-            dp[i] = i;
+            dp[i] = i; // 일단 최악의 경우, 거리만큼 이동한다고 초기 설정 (지름길을 전혀 사용하지 않는 경우)
         }
 
         for (int i=0; i<=d; i++){
-            dp[i] = Math.min(dp[i], (i>0) ? dp[i-1]+1: dp[i]);
+            dp[i] = Math.min(dp[i], (i>0) ? dp[i-1]+1 : dp[i]); // 현재 거리까지 오는데 필요한 최소 이동 거리를 계산
             for (int j=0; j<n; j++){
-                int s = arr[j][0];
-                int e = arr[j][1];
-                int w = arr[j][2];
-                if (i==s && e<=d){
-                    dp[e] = Math.min(dp[e], dp[i]+w);
+                int s = arr[j][0]; // 지름길의 시작점
+                int e = arr[j][1]; // 지름길의 끝점
+                int w = arr[j][2]; // 지름길을 이용했을 때 거리
+                if (i == s && e <= d){ // 현재 위치에서 지름길을 사용할 수 있는 경우
+                    dp[e] = Math.min(dp[e], dp[i] + w); // 지름길을 사용하여 끝점까지 가는 거리와 기존의 거리를 비교해 더 작은 값으로 업데이트
                 }
             }
         }
 
-        System.out.println(dp[d]);
-
+        System.out.println(dp[d]); // 목적지까지의 최소 이동 거리 출력
     }
 }
--- javaalgorithm/study/apr_4week/BJ1515HJ.java
+++ javaalgorithm/study/apr_4week/BJ1515HJ.java
@@ -1,19 +1,20 @@
 package study.apr_4week;
 
 import java.io.*;
-import java.util.*;
 
 public class BJ1515HJ {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String n = br.readLine();
+        String n = br.readLine();   // 숫자의 연속에서 찾고자 하는 특정 숫자열 입력
 
-        int p = 0;
-        int num = 0;
-        while (num ++ <= 30000){
-            String temp = String.valueOf(num);
-            for (int i=0; i<temp.length(); i++){
+        int p = 0;  // 입력된 문자열 `n`을 찾기 위해 사용하는 인덱스 포인터
+        int num = 0; // 현재 확인하고 있는 숫자
+        while (num++ <= 30000){    // 1부터 30000까지의 숫자를 확인합니다 (최대 3000자리를 고려하여 설정)
+            String temp = String.valueOf(num); // 현재 숫자를 문자열로 변환
+            for (int i=0; i<temp.length(); i++){ // 변환된 문자열의 각 자리수를 순회
+                // 현재 숫자의 i번째 자리와 입력받은 문자열 n의 p번째 자리가 같으면, p를 증가시켜 다음 문자로 넘어감
                 if (temp.charAt(i) == n.charAt(p)) p++;
+                // n의 모든 문자를 순회했다면, 현재 숫자를 출력하고 프로그램 종료
                 if (p == n.length()){
                     System.out.println(num);
                     System.exit(0);
